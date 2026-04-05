61d8856a0e02a6f9463a256ac168f8cfaffc0546
rakhikamboz7 <rakhikamboz7@gmail.com>
2026-04-06 02:55:23 +0530

    added event-listener

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-
 //
+
+const loadBtn = document.getElementById('loadBtn');
+const dataOutput = document.getElementById('data');
+
 function loadData() {
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(function(res) {
+
+  loadBtn.disabled =  true;
+  loadBtn.textContent = 'Loading...';
+  dataOutput.innerHTML = `<p class="status-message"> Fetching data...</p>`;
+
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(function (res) {
+      if(!res.ok) throw new Error('Network response was not ok');
       return res.json();
     })
 
     // var Code: The closure has a live link to a variable that changes before the closure is used.
-    // 
-    
-    .then(function(data) { 
-      console.log("This is data befroe loop:", data)
-       //outer function
-      for (let i = 0; i < data.length; i++) {  
-        console.log(i); 
-          // loop starts i=0  -- closure is like a live link of a variable
-        setTimeout(function() {  //inner function- using "data" from outer function 
-          document.getElementById("data").innerHTML += "<p>" + data[i].name + "</p>";
-         
-        console.log("This is data befroe loop:", data)
-        }, 1000);   
-      }
+    //
+
+    .then(function (data) {
+      console.log("This is data befroe loop:", data);
+      //outer function
+      data.forEach(function (user) {
+        // 'user' is a new variable for THIS specific turn
+        setTimeout(function () {
+          const userParagraph = document.createElement('p');
+          userParagraph.className = 'user-item';
+          userParagraph.textContent= user.name;
+
+          dataOutput.appendChild(userParagraph);
+          // This closure captures the 'user' variable
+          console.log("rendered user via closure",user.name);
+        }, 1000);
+      });
+      console.log("Loop initiated:", data);
     })
-    .catch(function(err) {
-      console.log(err);
+    .catch(function (err) {
+      dataOutput.innerHTML = `<p class="error-message"> Error: ${err.message}</p>`;
+      //handling network errors or parsing errors
+      console.error(err);
+    })
+    .finally(function() {
+      // Re-enable button after logic done
+      loadBtn.disabled= false;
+      loadBtn.textContent = 'Loaded Data'
+      if(data) {
+         dataOutput.innerHTML= "Below are User's names"
+      } 
+   
+
     });
+    
 }
+if(loadBtn){
+  loadBtn.addEventListener('click', loadData);
+ }
+
+
+
+ 
+
+
+
