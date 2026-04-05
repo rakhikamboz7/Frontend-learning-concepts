cfa9f45b66a4157a0206ad1bbd13749fb91ea01d
rakhikamboz7 <rakhikamboz7@gmail.com>
2026-04-06 02:11:32 +0530

    added console statements to see loop output

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-const loadButton = document.querySelector(".action-button");
-const dataContainer = document.getElementById("data");
 
-async function loadData() {
-  if (loadButton.disabled) return;
+//
+function loadData() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(res) {
+      return res.json();
+    })
 
-  loadButton.disabled = true;
-  dataContainer.textContent = "Loading...";
-
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-
-    const users = await response.json();
-
-    dataContainer.innerHTML = "";
-
-    const fragment = document.createDocumentFragment();
-
-    users.forEach((user, index) => {
-      const userElement = document.createElement("p");
-      userElement.textContent = user.name;
-
-      setTimeout(() => {
-        dataContainer.appendChild(userElement);
-      }, index * 300);
+    // var Code: The closure has a live link to a variable that changes before the closure is used.
+    // 
+    
+    .then(function(data) { 
+      console.log("This is data befroe loop:", data)
+       //outer function
+      for (let i = 0; i < data.length; i++) {  
+        console.log(i); 
+          // loop starts i=0  -- closure is like a live link of a variable
+        setTimeout(function() {  //inner function- using "data" from outer function 
+          document.getElementById("data").innerHTML += "<p>" + data[i].name + "</p>";
+         
+        console.log("This is data befroe loop:", data)
+        }, 1000);   
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
     });
-
-  } catch (error) {
-    dataContainer.textContent = "Failed to load data";
-  } finally {
-    setTimeout(() => {
-      loadButton.disabled = false;
-    }, 300);
-  }
 }
-
-loadButton.addEventListener("click", loadData);
